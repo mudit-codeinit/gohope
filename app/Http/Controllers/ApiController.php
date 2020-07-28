740894f79185a7bab9aef339b6960625e6724092
Ubuntu <ubuntu@ip-172-31-14-103.ap-south-1.compute.internal>
2020-07-28 07:50:58 +0000

    Check notification

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -628,47 +628,15 @@ class ApiController extends Controller
 		
 	public function push_notification(Request $request){ 
 		  //API URL of FCM
-		    $url = 'https://fcm.googleapis.com/fcm/send';
-
-		    /*api_key available in:
-		    Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key*/    
-			
-		 $api_key = isset($request->api_key) && !empty($request->api_key) ? $request->api_key :'AAAAmgHa4eM:APA91bF0cMs1LiRD7TuF1YJHcUkR2nnrDWLaWde6z48f-al05jkEnBieEN_DhsHh4Yw_gBZ0rNU_OIxsynAi1s2ATR5hXHmOIpMxyv2MqcRhz5xtdgY9CLIj6nCNFC6TXZhpqC4CV0k9';
-			 
+		   $message = isset($request->message) && !empty($request->message) ? $request->message :'this is a test';
 		    
-			
-		 $message = isset($request->message) && !empty($request->message) ? $request->message :'this is a test';
-		    $fields = array (
-			'registration_ids' => array (
-			     $request->device_token
-			),
-			'data' => array (
-				"message" => $message
-			)
-		    );
-
-		    //header includes Content type and api key
-		    $headers = array(
-			'Content-Type:application/json',
-			'Authorization:key='.$api_key
-		    );
-				
-		    $ch = curl_init();
-		    curl_setopt($ch, CURLOPT_URL, $url);
-		    curl_setopt($ch, CURLOPT_POST, true);
-		    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-		    $result = curl_exec($ch);
-		    if ($result === FALSE) {
-			die('FCM Send Error: ' . curl_error($ch));
-		    }
-		    curl_close($ch);
-		    return $result;
-		
-		
+			if($request->notification ==  'reporter'){
+					$result =  push_repoter_notification_android( $request->device_token ,$message ); 
+			}
+			if($request->notification ==  'driver'){
+				$result = 	push_driver_notification_android( $request->device_token ,$message ); 
+			}
+			return $result;
 	}
 	
 }
